15e7f3149741a940ecb5d48682dfdf156650c25a
Ömer <omersoydinc3232@gmail.com>
2022-10-24 22:07:55 +0300

    Add files via upload

---
+++ Siniflar3Kapsulleme/src/Product.java
@@ -0,0 +1,14 @@
+public class Product {
+    
+    private int ID;
+    private String name;
+    private String description;
+    private double primce;
+    private int stokAmount;
+    private String renk;
+          
+    
+    public void metod (){
+        //System.out.println(ID);
+    }
+}
---
+++ Siniflar3Kapsulleme/src/ProductManager.java
@@ -0,0 +1,13 @@
+public class ProductManager {
+    
+    public void Add( Product product){
+        // System.out.println("Ürün eklendi " + product.name);    //product.name private tanımladığımız için hata veriyor.
+        
+    }
+    
+    public void Add2(int ID,String name,String 
+            discribtion,int stockAmount,double price){
+        
+        
+    }
+}
