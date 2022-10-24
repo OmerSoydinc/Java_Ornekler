d82ec3f915c0b37554ffced2680b0c4391483c9e
Ömer <omersoydinc3232@gmail.com>
2022-10-24 22:07:06 +0300

    Add files via upload

---
+++ Siniflar2/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+   
+    public static void main(String[] args) {
+        
+        DortIslem dortIslem=new DortIslem();
+        
+        int sonuc1=dortIslem.Topla(12,4);
+        System.out.println(sonuc1);
+        
+        int sonuc2=dortIslem.Cikar(12,4);
+        System.out.println(sonuc2);
+        
+        int sonuc3=dortIslem.Carp(12,4);
+        System.out.println(sonuc3);
+        
+        int sonuc4=dortIslem.Bol(12,4);
+        System.out.println(sonuc4);
+        
+        
+    }
+    
+}
