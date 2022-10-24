f6241b57e364ec6a53c5a2a088685f854ee72b52
Ömer <omersoydinc3232@gmail.com>
2022-10-24 22:05:56 +0300

    Add files via upload

---
+++ Siniflar/src/CustomerManager.java
@@ -0,0 +1,15 @@
+public class CustomerManager {
+    
+    public void Add(){
+        System.out.println("Müsteri Eklendi");
+    }
+    
+    public void Remove(){
+        System.out.println("Müsteri Silindi");
+    }
+    
+    public void UpDate(){
+        System.out.println("Müsteri Güncellendi");
+    }
+    
+}
---
+++ Siniflar/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        //referans tip
+        
+       CustomerManager customerManager = new CustomerManager();
+       
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.UpDate();
+        
+        CustomerManager customerManager2 = new CustomerManager();
+        
+        customerManager2.Add();
+        customerManager2.Remove();
+        customerManager2.UpDate();
+        
+        
+    }
+    
+}
