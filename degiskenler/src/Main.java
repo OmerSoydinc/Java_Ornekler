ab2183a230d2bca88a592f53357d828b101259fa
Ömer <omersoydinc3232@gmail.com>
2022-10-24 21:57:22 +0300

    Add files via upload

---
+++ degiskenler/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+      
+        int a=9;
+        System.out.println("Öğrenci Sayımı: " + a);
+        
+        
+        System.out.println("Öğrenci Sayımı: " + a++); //yaz sonra arttır        
+        
+        System.out.println("Öğrenci Sayımı: " + ++a);  //arttır sonra yaz
+        
+        String mesaj="Öğrenci Sayısı: ";
+        
+        a=50;
+        System.out.println(mesaj+a);
+        
+         char karakter='a';
+         
+         double dab=115.8;
+         
+         float a1= 18;
+         
+         boolean dogruMu=true;
+         
+         
+        
+    }
+    
+}
