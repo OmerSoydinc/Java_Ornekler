1a8d17145a0594b9a2cd907bb06b7b648cdac106
Ömer <omersoydinc3232@gmail.com>
2022-11-30 00:55:05 +0300

    Delete AbstractClasesDenme directory

--- AbstractClasesDenme/src/SqlServerDatabaseManager.java
+++
@@ -1,7 +0,0 @@
-public class SqlServerDatabaseManager extends BaseDatabaseManager{
-    
-    @Override
-    public void getData(){
-        System.out.println("Veri Getirildi: SQL SERVER");
-    }
-}
