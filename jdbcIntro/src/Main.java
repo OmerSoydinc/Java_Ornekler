4f3d477ed83200bf6c3b2197ae6e9aab0f6c4f5c
Ömer <omersoydinc3232@gmail.com>
2022-12-31 00:42:16 +0300

    Update Main.java

--- jdbcIntro/src/Main.java
+++ jdbcIntro/src/Main.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.*;
 import java.util.ArrayList;
-public class Main {   //netbeans add jar
+public class Main {   //netbeans add jar  (netbeans kullanımı için jar dosyasını eklemek için kaynak taraması yapılabilir)
     public static void main(String[] args) throws SQLException{
         
         selectDemo();     //Kayıt Getirir-Okur
