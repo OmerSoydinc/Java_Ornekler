dd6b92b15c52afb23c82fc9a3acc10d36c15adb6
Ömer <omersoydinc3232@gmail.com>
2023-01-01 02:35:04 +0300

    Add files via upload

---
+++ firstSwingDemo/src/frmIntro.java
@@ -0,0 +1,100 @@
+
+
+//butonu eklediğimiz için arka tarafta bir class oluşmuştur.
+public class frmIntro extends javax.swing.JFrame {
+
+    
+    public frmIntro() {
+         //initialize kısaltamsı gibi düşün
+         
+        initComponents();   
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnMessage1 = new javax.swing.JButton();
+        btnMessage2 = new javax.swing.JButton();
+        lblMessage = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btnMessage1.setBackground(new java.awt.Color(51, 255, 204));
+        btnMessage1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        btnMessage1.setText("Selam Ver 1");
+        btnMessage1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMessage1ActionPerformed(evt);
+            }
+        });
+
+        btnMessage2.setBackground(new java.awt.Color(255, 0, 0));
+        btnMessage2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        btnMessage2.setText("Selam Ver 2");
+        btnMessage2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMessage2ActionPerformed(evt);
+            }
+        });
+
+        lblMessage.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(134, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(lblMessage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(btnMessage1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnMessage2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap(135, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(86, 86, 86)
+                .addComponent(btnMessage1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnMessage2, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblMessage, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(68, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnMessage1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMessage1ActionPerformed
+        //buton 1 e tıklandığında yapılacak iş
+        
+        System.out.println("Merhaba 1...");
+    }//GEN-LAST:event_btnMessage1ActionPerformed
+
+    private void btnMessage2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMessage2ActionPerformed
+        //buton 2 ye tıklandığında yapılacak iş
+        // buton 2 ye basarsam label a aşağıdaki mesajı yazar 
+        lblMessage.setText("Merhaba2...");
+    }//GEN-LAST:event_btnMessage2ActionPerformed
+
+    
+    public static void main(String args[]) {
+       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new frmIntro().setVisible(true);
+            }
+        });
+    }
+    
+    //modify etme demektedir.
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnMessage1;
+    private javax.swing.JButton btnMessage2;
+    private javax.swing.JLabel lblMessage;
+    // End of variables declaration//GEN-END:variables
+}
